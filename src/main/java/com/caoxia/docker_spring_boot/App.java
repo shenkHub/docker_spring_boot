6388724e339dc2391d1f766df47c7cd5748e9312
shenkHub <2388689831@qq.com>
2018-02-27 17:45:34 +0800

    Initial commit

---
+++ src/main/java/com/caoxia/docker_spring_boot/App.java
@@ -0,0 +1,19 @@
+package com.caoxia.docker_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class App {
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+	@GetMapping("/")
+	public String index() {
+		return "你好,docker";
+	}
+}
